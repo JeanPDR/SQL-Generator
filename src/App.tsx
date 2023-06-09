fe66cd8d6636f967844948a448f9b9bbb085f593
Jean Pereira dos Reis <jeanpereira.jpdr@gmail.com>
2023-06-09 15:24:29 +0000

    UPDATE - Realizando a criação dos components e finalizando front

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+import MessagesDisplay from './components/MessagesDisplay';
+import CodeDisplay from './components/CodeDisplay';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <MessagesDisplay />
+      <input />
+      <CodeDisplay />
+      <div className="button-container">
+        <button id="get-query">obter consulta</button>
+        <button id="clear-chat">limpar conversa</button>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MessageDisplay = () => {
+    return (
+      <div className="message-display">
+        <p className="icon">X</p>
+        <p>usuário</p>
+        <p>mensagem</p>
+      </div>
+    );
+  };
+  
+  export default MessageDisplay;
