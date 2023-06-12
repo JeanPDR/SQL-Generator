0b0f17af4c927eedf53772ad974d7d8a4b39b095
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2023-06-12 20:53:14 -0300

    UPDATE - Alterações no index.ts

--- index.ts
+++ index.ts
@@ -1,36 +1,34 @@
-import express, {Application, Request, Response} from "express"
-import cors from "cors"
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
-const PORT: number = 4000
 
 const app: Application = express();
+const PORT = 4000;
+
 app.use(cors());
 app.use(express.json());
 
 const API_KEY: string = "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";
 
-const configuration = new Configuration({
-    apiKey: API_KEY
-})
-
-const openai = new OpenAIApi(configuration)
+const configuration = new Configuration({ apiKey: API_KEY });
+const openai = new OpenAIApi(configuration);
 
 app.post("/completions", async (req: Request, res: Response) => {
-    try {
-        const completion = await openai.createChatCompletion({
-            model: "gpt-4",
-            messages: [
-                {
-                    role: "user", 
-                    content: "Create a SQL request to" + req.body.message
-                }]
-        })
-        res.send(completion.data.choices[0].message);
-    }catch (error){
-        console.error(error)
-        res.status(500).send("Server Error")
-    }
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "user",
+          content: `Create a SQL request to ${req.body.message}`
+        }
+      ]
+    });
+    res.send(completion.data.choices[0].message);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 
-})
-
-app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`));
