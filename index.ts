0b62a0c626467626ef904e6fd651c7ae857fdd96
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2023-06-12 20:31:26 -0300

    UPDATE - Realizando teste com a api do chat gpt v4

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express, {Application, Request, Response} from "express"
+import cors from "cors"
+import { Configuration, OpenAIApi } from "openai";
+const PORT: number = 4000
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+const API_KEY: string = "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";
+
+const configuration = new Configuration({
+    apiKey: API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+app.post("/completions", async (req: Request, res: Response) => {
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-4",
+            messages: [
+                {
+                    role: "user", 
+                    content: "Create a SQL request to" + req.body.message
+                }]
+        })
+        res.send(completion.data.choices[0].message);
+    }catch (error){
+        console.error(error)
+        res.status(500).send("Server Error")
+    }
+
+})
+
+app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`))
--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,32 @@ import MessagesDisplay from './components/MessagesDisplay';
 import CodeDisplay from './components/CodeDisplay';
 
 export default function App() {
+
+  const getQuery = async () => {
+    try{
+       const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          message:"create a table"
+        })
+       }
+       const response = await fetch("https:sql.jeanpdr.dev:4000/completions", options)
+       const data = await response.json()
+       console.log(data)
+    }catch (error){
+      console.error(error);
+    }
+  }
   return (
     <div className="app">
       <MessagesDisplay />
       <input />
       <CodeDisplay />
       <div className="button-container">
-        <button id="get-query">obter consulta</button>
+        <button id="get-query" onClick={getQuery}>obter consulta</button>
         <button id="clear-chat">limpar conversa</button>
       </div>
     </div>
