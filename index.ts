d827e52a9c8de5f3027ffa552a97643551f0987f
Jean Pereira dos Reis <90737370+JeanPDR@users.noreply.github.com>
2023-06-13 12:07:17 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -18,14 +18,9 @@ const openai = new OpenAIApi(configuration)
 app.post("/completions", async (req: Request, res: Response) => {
   try {
     const completion = await openai.createChatCompletion({
-    model: "text-davinci-003",
-      messages: [
-        {
-          role: "user",
-          content: "Create a SQL request to" + req.body.message
-        }
-      ]
-    });
+   model: "gpt-3.5-turbo",
+  messages: [{"role": "user", "content": "Hello!"}]
+    })
     res.send(completion.data.choices[0].message);
   } catch (error) {
     console.error(error);
